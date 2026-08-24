dd2dc6e542f6f34d83fd2968be62f20591bf3292
Liam <liam@scalzulli.com>
2026-08-23 22:46:48 -0400

    Remove brittle OpenAPI shape test

--- src/documentation.rs
+++ src/documentation.rs
@@ -19,33 +19,3 @@ use super::*;
   ),
 )]
 pub(crate) struct Documentation;
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn documents_the_read_api() {
-    let document = serde_json::to_value(Documentation::openapi()).unwrap();
-    let paths = document["paths"].as_object().unwrap();
-
-    assert!(paths.contains_key("/healthz"));
-    assert!(paths.contains_key("/v1/jobs"));
-    assert!(paths.contains_key("/v1/jobs/{id}"));
-    assert!(paths.contains_key("/v1/sources"));
-
-    let parameters = paths["/v1/jobs"]["get"]["parameters"]
-      .as_array()
-      .unwrap()
-      .iter()
-      .map(|parameter| parameter["name"].as_str().unwrap())
-      .collect::<Vec<_>>();
-
-    assert_eq!(parameters, ["cursor", "limit", "query", "remote", "source"],);
-    assert!(
-      paths["/v1/jobs/{id}"]["get"]["responses"]
-        .get("404")
-        .is_some()
-    );
-  }
-}
